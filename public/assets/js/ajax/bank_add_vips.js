bdea9d1be20afb627b9038bf98210838032f0d73
d15118080 <66905665+d15118080@users.noreply.github.com>
2022-12-19 05:48:07 +0900

    Create bank_add_vips.js

---
+++ public/assets/js/ajax/bank_add_vips.js
@@ -0,0 +1,105 @@
+let route_id = $('#route_id').val();
+let company_id = $('#company_id').val();
+
+function Won_shipment() {
+    Swal.fire({
+        title:"잠시만 기다려주세요",
+        allowEscapeKey: false,
+        allowOutsideClick: false,
+        didOpen: () => {
+            Swal.showLoading();
+        },
+    });
+    $.ajax({
+        type: "POST",
+        url: '/api/v1/user/1won_shipment/' + route_id,
+        data: {
+            bankCode: $('#bank_code').val(),
+            acctNo: $('#bank_number').val(),
+            custNm: $('#bank_user_name').val()
+        },
+        success: function (res) {
+            $("#one").css("display", "none");
+            $("#two").css("display", "");
+            $.cookie("verifyTrDt", res.data.verifyTrDt);
+            $.cookie("verifyTrNo", res.data.verifyTrNo);
+            Swal.close()
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            let data = XMLHttpRequest.responseJSON;
+            Swal.fire({
+                icon: "error",
+                title: `에러(${data.result.resultCd})`,
+                text: data.result.advanceMsg,
+            });
+        },
+    });
+}
+
+function Won_shipment_check() {
+    Swal.fire({
+        title:"잠시만 기다려주세요",
+        allowEscapeKey: false,
+        allowOutsideClick: false,
+        didOpen: () => {
+            Swal.showLoading();
+        },
+    });
+    $.ajax({
+        type: "POST",
+        url: '/api/v1/user/1won_shipment_check/' + route_id,
+        data: {
+            verifyTrDt:$.cookie("verifyTrDt"),
+            verifyTrNo:$.cookie("verifyTrNo"),
+            verifyVal:$('#verifyVal').val()
+        },
+        success: function (res) {
+            $("#two").css("display", "none");
+            $("#three").css("display", "");
+            Account_everlasting_issuance()
+            Swal.close()
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            let data = XMLHttpRequest.responseJSON;
+            Swal.fire({
+                icon: "error",
+                title: `에러(${data.result.resultCd})`,
+                text: data.result.advanceMsg,
+            });
+        },
+    });
+}
+
+function Account_everlasting_issuance() {
+    Swal.fire({
+        title:"잠시만 기다려주세요",
+        allowEscapeKey: false,
+        allowOutsideClick: false,
+        didOpen: () => {
+            Swal.showLoading();
+        },
+    });
+    $.ajax({
+        type: "POST",
+        url: '/api/v1/user/account_everlasting_issuance/' + route_id+"/"+company_id,
+        data: {
+            bankCode: $('#bank_code').val(),
+            acctNo: $('#bank_number').val(),
+            custNm: $('#bank_user_name').val(),
+        },
+        success: function (res) {
+            $("#three").css("display", "none");
+            $("#for").css("display", "");
+            $("#bankAcctNo").val(res.data.bank_no);
+            Swal.close()
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            let data = XMLHttpRequest.responseJSON;
+            Swal.fire({
+                icon: "error",
+                title: `에러(${data.result.resultCd})`,
+                text: data.result.advanceMsg,
+            });
+        },
+    });
+}
