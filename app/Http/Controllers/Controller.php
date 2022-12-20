e6f2657434adf049cb2fea6212a22ce134ad12b0
d15118080 <66905665+d15118080@users.noreply.github.com>
2022-12-21 04:46:54 +0900

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -427,7 +427,7 @@ class Controller extends BaseController
         }else{
             $company_key = User::where('key', $HToken)->value('company_key');
             if($mode == 1){
-                $data = company::where('state',1)->where('head_key',$company_key)->get();
+                $data = company::where('state',1)->where('company_key',$company_key)->get();
             }elseif ($mode == 2){
                 $data = company::where('state',2)->where('head_key',$company_key)->get();
             }elseif ($mode == 3){
